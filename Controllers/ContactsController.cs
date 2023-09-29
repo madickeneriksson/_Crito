c09dc8ea8dabc5699838af21d20e9893b8cfe937
Conlerc <conlerc@live.se>
2023-09-29 11:56:47 +0200

    Add project files.

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,35 @@
+﻿using Crito.Models;
+using Crito.Services;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+namespace Crito.Controllers
+{
+    public class ContactsController : SurfaceController
+    {
+        public ContactsController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactForm contactForm)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            using var mail = new MailService("no-reaoly@crito.com", "smtp.crito.com", 465, "support@crito.com", "Bytmig123!");
+            //to sender
+            mail.SendAsync(contactForm.Email, "Our request was received", "Hi your request was received, we will contact you").ConfigureAwait(false);
+            // to us
+            mail.SendAsync("support@crito.com", $"{contactForm.Name} sent a contact request", contactForm.Message).ConfigureAwait(false);
+
+
+            return LocalRedirect(contactForm.ReadirectUrl ?? "/");
+        }
+    }
+}
---
+++ Models/ContactForm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crito.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public string? ReadirectUrl { get; set; } = "/contacts";
+    }
+}
